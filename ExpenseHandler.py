44825911be84703ff9ae3a7e2a9b2d26682fee24
tazeek <tazeek.rakib@gmail.com>
2020-11-09 18:54:09 +0800

    Zero expenses filled in

--- ExpenseHandler.py
+++ ExpenseHandler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 class ExpenseHandler:
 
@@ -13,3 +14,32 @@ class ExpenseHandler:
 	def getExpenseDF(self):
 
 		return self._expenses_df
+
+	def _zeroExpenseHelper(self,date):
+		return {
+			'date': date,
+			'category': 'zero expenses',
+			'type': 'none',
+			'cost': 0.00
+		}
+
+	def fillZeroExpenseDates(self):
+
+		expenses_df = self._expenses_df
+
+		earliest_date = expenses_df['date'].iloc[0].date()
+		today_date = datetime.now().date()
+
+		# Find the days when there were no expenses
+		date_range_df = pd.date_range(start=earliest_date, end=today_date)
+		zero_expense_dates = date_range_df.difference(expenses_df['date'])
+
+		zero_expense_dict_list = [
+			self._zeroExpenseHelper(date) for date in zero_expense_dates
+		]
+
+		expenses_df = expenses_df.append(zero_expense_dict_list, ignore_index=True)
+		expenses_df.sort_values(by='date',ascending=True, inplace=True)
+		expenses_df.reset_index(drop=True,inplace=True)
+
+		self._expenses_df = expenses_df
