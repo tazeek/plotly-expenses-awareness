29ac6ff166907ed35061e270f489ba68f556f0dd
tazeek <tazeek.rakib@gmail.com>
2020-11-09 18:36:58 +0800

    Initializer added

---
+++ ExpenseHandler.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class ExpenseHandler:
+
+	def __init__(self):
+
+		file = 'folder/expenses.csv'
+		expenses_df = pd.read_csv('folder/expenses.csv')
+		expenses_df['date'] = pd.to_datetime(expenses_df['date'])
+
+		self._expenses_df = expenses_df
+
+	def getExpenseDF(self):
+
+		return self._expenses_df
