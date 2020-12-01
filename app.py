eb5ee2dbd0537f950d1a5e093d3a0196e9f915c3
tazeek <tazeek.rakib@gmail.com>
2020-12-02 07:41:53 +0800

    ExpenseHandler object added

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+from ExpenseHandler import ExpenseHandler
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-def initialize_app():
+def initialize_app(app):
 
-	app = Dash.dash()
+	expenses_obj = ExpenseHandler()
+
+	# Time series plot by day
+	costs_df = expenses_obj.get_total_costs('date')
 
 if __name__ == '__main__':
 
 	app = Dash.dash()
-	
+
 	initialize_app(app)
 
 	app.run_server(debug=True)
