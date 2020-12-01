5cdb762e27a62be4ddd13a10c9daa904f839005c
tazeek <tazeek.rakib@gmail.com>
2020-12-02 07:46:10 +0800

    Graph generator code added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from ExpenseHandler import ExpenseHandler
 
+import plotly.graph_objects as go
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -10,6 +12,23 @@ def initialize_app(app):
 
 	# Time series plot by day
 	costs_df = expenses_obj.get_total_costs('date')
+
+	daily_expense_fig = go.Figure([
+		go.Scatter(
+			x=costs_df['date'],
+			y=costs_df['cost'],
+			mode='lines+markets'
+		)
+	])
+
+	daily_expense_fig.update_layout(title_text='Overview of expenses (Daily)')
+	daily_expense_fig.update_xaxes(rangeslider_visible=True)
+
+	app.layout = html.Div([
+		dcc.Graph(id='daily-expense-total',figure=daily_expense_fig)
+	])
+
+	return None
 
 if __name__ == '__main__':
 
