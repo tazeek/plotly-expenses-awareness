c80205dd3ebf6664715b7d64bc5a22606e8ec7f6
tazeek <tazeek.rakib@gmail.com>
2020-12-04 07:59:27 +0800

    Daily expense code added

--- Graphs.py
+++ Graphs.py
@@ -7,3 +7,24 @@ class Graphs:
 	def __init__(self):
 
 		self._expense_obj = ExpenseHandler()
+
+	def get_daily_expenses_total(self):
+
+		expense_obj = self._expense_obj
+
+		daily_costs_df = expenses_obj.get_total_costs('date')
+
+		daily_expense_fig = go.Figure([
+			go.Scatter(
+				x=daily_costs_df['date'],
+				y=daily_costs_df['cost'],
+				mode='lines+markers'
+			)
+		])
+
+		daily_expense_fig.update_layout(title_text='Overview of expenses (Daily)')
+		daily_expense_fig.update_xaxes(rangeslider_visible=True)
+
+		return daily_expense_fig
+
+	def get_monthly_expenses_total(self):
