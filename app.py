334c0c318a16560edf6dd65f2d5a51a83b889d65
tazeek <tazeek.rakib@gmail.com>
2020-12-10 08:08:00 +0800

    Re-ordered graph display

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def initialize_app(app):
 	graphs_obj = Graphs()
 
 	app.layout = html.Div([
-		dcc.Graph(id='daily-expense-figure',figure=graphs_obj.get_daily_expenses_fig()),
-		dcc.Graph(id='last-30days-figure',figure=graphs_obj.get_last_days_expenses(30)),
 		dcc.Graph(id='last-7days-figure',figure=graphs_obj.get_last_days_expenses(7)),
+		dcc.Graph(id='last-30days-figure',figure=graphs_obj.get_last_days_expenses(30)),
+		dcc.Graph(id='daily-expense-figure',figure=graphs_obj.get_daily_expenses_fig()),
 		dcc.Graph(id='monthly-expense-total',figure=graphs_obj.get_monthly_expenses_fig()),
 		dcc.Graph(id='daily-average-calculation',figure=graphs_obj.get_day_averages_fig()),
 	])
