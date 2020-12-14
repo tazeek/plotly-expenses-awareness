6cf491559685649714e06bc17a9a13511584378b
tazeek <tazeek.rakib@gmail.com>
2020-12-15 07:33:26 +0800

    Removed the extra graphs

--- app.py
+++ app.py
@@ -22,10 +22,8 @@ def initialize_app():
 			labelStyle={'display':'inline-block'}
 		),
 
-		dcc.Graph(id='expense-days-figure'),
+		dcc.Graph(id='expense-days-figure',figure=graphs_obj.get_last_days_expenses(7)),
 
-		dcc.Graph(id='last-7days-figure',figure=graphs_obj.get_last_days_expenses(7)),
-		dcc.Graph(id='last-30days-figure',figure=graphs_obj.get_last_days_expenses(30)),
 		dcc.Graph(id='daily-expense-figure',figure=graphs_obj.get_daily_expenses_fig()),
 		dcc.Graph(id='monthly-expense-total',figure=graphs_obj.get_monthly_expenses_fig()),
 		dcc.Graph(id='daily-average-calculation',figure=graphs_obj.get_day_averages_fig()),
