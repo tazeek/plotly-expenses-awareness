90ec2378386882ebc7eef13421886a6d0831a675
tazeek <tazeek.rakib@gmail.com>
2020-12-21 07:36:01 +0800

    New function added in Graphs.py

--- Graphs.py
+++ Graphs.py
@@ -46,6 +46,34 @@ class Graphs:
 
 		return fig
 
+	def get_expenses_filter_days(self, last_n_days, start_date, end_date):
+
+		df = self._expense_obj.filter_expenses_dates(last_n_days, start_date, end_date)
+		total, average = df['cost'].sum(), df['cost'].mean()
+
+		total_str = f'Total spent: {total:.2f}'
+		mean_str = f'Average per day: {average:.2f}'
+
+		fig = go.Figure([
+			go.Scatter(
+				x=df['date'],
+				y=df['cost'],
+				mode='lines+markers'
+			)
+		])
+
+		fig.update_layout(
+			title_text='Overview of expenses (Total per day)',
+			transition = 
+				{
+					'duration': 500,
+					'easing': 'linear'
+				}
+		)
+
+		return fig,total_str,mean_str
+
+
 	def get_last_days_expenses(self, last_n_days):
 
 		df = self._expense_obj.filter_expenses_dates(last_n_days)
