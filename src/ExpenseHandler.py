b0d152679cf7811ae5b73e0cd655bed051ab1a02
tazeek <tazeek.rakib@gmail.com>
2021-02-10 16:28:24 +0800

    Return new dataframe

--- src/ExpenseHandler.py
+++ src/ExpenseHandler.py
@@ -215,8 +215,12 @@ class ExpenseHandler:
 	def count_monthly_average(self):
 		expense_df = self.get_expense_stats('daily')[['date','cost']]
 		expense_df.set_index('date',inplace=True)
-		
+
 		avg_df = expense_df.resample('M').mean()
 		non_zero_avg_df = expense_df[expense_df['cost'] > 0].resample('M').mean()
 
-		return avg_df, non_zero_avg_df
+		return pd.DataFrame({
+			'date': avg_df.index.strftime("%b-%Y"),
+			'full_average': avg_df['cost'],
+			'non_zero_average': non_zero_avg_df['cost']
+		})
